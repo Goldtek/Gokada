0f84418b7ddae6a1ae27f3cd7b8fe862bfb1bd30
Goldtek <dike4mii4real@gmail.com>
2021-03-18 01:43:26 +0100

    cleanup

--- src/lib/appNavigator.js
+++ src/lib/appNavigator.js
@@ -3,7 +3,6 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Users from "../components/screens/users";
 import Logs from "../components/screens/logs";
-import { colors } from '../components/styles';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,8 +11,5 @@ export const PublicNavigator = () => (
   <Drawer.Navigator initialRouteName="Users">
     <Drawer.Screen name="Users" component={Users} />
     <Drawer.Screen name="Logs" component={Logs} />
-  
-    {/* <Drawer.Screen name="EditProfile" component={EditProfile} />
-    <Drawer.Screen name="Messages" component={Messages} /> */}
   </Drawer.Navigator>
 );
